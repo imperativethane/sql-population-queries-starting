3782f77714a8fae43d888b057fdbac8e91cd6ce7
Derek Millar <derek.millartech@gmail.com>
2020-04-15 14:43:13 +0100

    Add SQL Queries

--- population_queries.sql
+++ population_queries.sql
@@ -4,3 +4,28 @@ SELECT DISTINCT year from population_years;
 
 -- Add your additional queries below:
 
+SELECT population, year
+FROM population_years
+WHERE country = 'Gabon'
+ORDER BY population DESC
+LIMIT 1;
+
+SELECT country, population
+FROM population_years
+WHERE year = 2005
+ORDER BY population
+LIMIT 10;
+
+SELECT DISTINCT country, population
+FROM population_years
+WHERE population > 100
+	AND year = 2010;
+	
+SELECT DISTINCT country
+FROM population_years
+WHERE country LIKE '%Islands%';
+
+SELECT DISTINCT country, population
+FROM population_years
+WHERE country = 'Indonesia'
+	AND (year = 2000 OR year = 2010);
